0ba91b172eb520c4e2c518828744ad045aafa948
Mayan Rothstein <mayan.rothstein@umusic.com>
2022-03-06 15:57:34 -0800

    toggle which part of the accordion is visable

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
+    // names the first element (index of 0) to the variable of 'activeIndex' and the second element (index of 1) to the variable of 'setActiveIndex'. null is the default value...so activeIndex is null in this case:
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const onTitleClick = (ind) => {
+        setActiveIndex(ind);
+    };
+
+    const renderedItems = items.map((item, ind) => {
+        const active = ind === activeIndex ? 'active': ''
         return (
             <React.Fragment key={item.title}>
-                <div className="title active">
+                <div 
+                    className={`title ${active}`}
+                    onClick={() => onTitleClick(ind) }>
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
