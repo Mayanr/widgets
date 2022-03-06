96874acc85be1a5fb6219723094b0471b5f35643
Mayan Rothstein <mayan.rothstein@umusic.com>
2022-03-06 14:26:06 -0800

    implement accordion styling and placeholder content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Accordion from './components/Accordion'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a FE JS framework.'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite among software engineers.'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by create components.'
+    }
+]
+export default () => {
+    return <Accordion items={items} />
+};
